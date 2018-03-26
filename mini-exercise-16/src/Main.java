67fa9d3b987d2a7bd80eb57623ffbeb0365f3886
Hieu <hieuci88@gmail.com>
2018-03-26 09:46:50 +0200

    feat: write function and run with example fields

--- mini-exercise-16/src/Main.java
+++ mini-exercise-16/src/Main.java
@@ -1,12 +1,23 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        ArrayList<String> listOfStrings = new ArrayList<>();
-        char letterToJoin;
+        ArrayList<String> listOfStrings = new ArrayList<>(Arrays.asList("ab", "cde", "fg"));
+        char letterToJoin = ' ';
+
+        System.out.println(joinStringsWithLetter(listOfStrings, letterToJoin));
 
     }
-    
+
+    public static String joinStringsWithLetter(ArrayList<String> inputListofStrings, char letterToJoin) {
+        String StringJoinedWithLetter = "";
+
+        for (int i = 0; i < inputListofStrings.size(); i++) {
+            StringJoinedWithLetter += inputListofStrings.get(i) + letterToJoin;
+        }
+        return StringJoinedWithLetter;
+    }
 }
