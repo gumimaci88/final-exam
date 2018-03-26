03e93107e8c6c86518b814f38b9054c592145029
Hieu <hieuci88@gmail.com>
2018-03-26 10:05:34 +0200

    feat: final touches, refactor

--- mini-exercise-16/src/Main.java
+++ mini-exercise-16/src/Main.java
@@ -1,22 +1,19 @@
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        ArrayList<String> listOfStrings = new ArrayList<>(Arrays.asList("ab", "cde", "fg"));
-        char letterToJoin = 'a';
+        ArrayList<String> listOfStrings = new ArrayList<String>(Arrays.asList("Boys", "will", "be", "boys."));
+        char letterToJoin = '-';
 
         System.out.println(joinStringsWithLetter(listOfStrings, letterToJoin));
-
     }
 
-    public static String joinStringsWithLetter(ArrayList<String> inputListofStrings, char letterToJoin) {
+    public static String joinStringsWithLetter(ArrayList<String> inputListofStrings, char inputLetterToJoin) {
         String StringJoinedWithLetter = "";
 
         for (int i = 0; i < inputListofStrings.size()-1; i++) {
-            StringJoinedWithLetter += inputListofStrings.get(i) + letterToJoin;
+            StringJoinedWithLetter += inputListofStrings.get(i) + inputLetterToJoin;
         }
 
         StringJoinedWithLetter += inputListofStrings.get(inputListofStrings.size()-1);
