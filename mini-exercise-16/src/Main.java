020e5edff077a890445639affbb0afd8bccfa662
Hieu <hieuci88@gmail.com>
2018-03-26 09:32:13 +0200

    feat: create initial fields

--- mini-exercise-16/src/Main.java
+++ mini-exercise-16/src/Main.java
@@ -1,5 +1,12 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        ArrayList<String> listOfStrings = new ArrayList<>();
+        char letterToJoin;
 
     }
+    
 }
